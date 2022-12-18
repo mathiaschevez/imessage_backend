de71ab71f0b4bdf2349ba9a46e4d48f2aa5277ef
Mathias Chevez <solo@Mathiass-MacBook-Air.local>
2022-12-18 00:58:06 -0700

    server now handling cors

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,67 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
-import typeDefs from './graphql/typeDefs/index.js';
-import resolvers from './graphql/resolvers/index.js';
-import { makeExecutableSchema } from '@graphql-tools/schema';
+// import { ApolloServer } from '@apollo/server';
+// import { startStandaloneServer } from '@apollo/server/standalone';
 
-const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers
-})
+// const server = new ApolloServer({ schema })
+
+// const { url } = await startStandaloneServer(server, {
+//   listen: { port: 4000 },
+// })
+
+// console.log(`🚀  Server ready at: ${url}`)
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({
-  schema
-})
 
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-})
 
-console.log(`🚀  Server ready at: ${url}`)
+// npm install @apollo/server express graphql cors body-parser
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import typeDefs from './graphql/typeDefs/index.js';
+import resolvers from './graphql/resolvers/index.js';
+import * as dotenv from 'dotenv'
+
+async function main() {
+  dotenv.config()
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const server = new ApolloServer({
+    schema,
+    csrfPrevention: true,
+    cache: 'bounded',
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer }),],
+  });
+
+  const corsOptions = {
+    origin: process.env.CLIENT_ORIGIN,
+    credentials: true,
+  }
+
+  await server.start();
+
+  app.use(
+    '/',
+    cors<cors.CorsRequest>(corsOptions),
+    bodyParser.json({ limit: '50mb' }),
+    expressMiddleware(server, {
+      context: async ({ req }) => ({ token: req.headers.token }),
+    }),
+  );
+
+  // Modified server startup
+  await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/`);
+}
+
+main().catch((err) => console.log(err))
